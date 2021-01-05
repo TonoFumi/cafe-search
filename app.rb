c8859a85126ae8dc695d181878a98bf410508a1f
FumiakiTono <tonotorious@gmail.com>
2021-01-05 19:15:23 +0900

    修正

--- app.rb
+++ app.rb
@@ -44,7 +44,7 @@ post '/callback' do
         # ログ出力設定
         master_res = req.get(master_query)
         #puts master_res.body
-        puts master_res.body['genre'][0]['code'] # エラー
+        puts master_res.body['results']['genre'][0]['code'] # エラー
         code = master_res.body['genre'][0]['code'] # エラー
 
         # 緯度経度情報をホットペッパーAPIに投げ近くのカフェ情報をLINEクライアントに返す
@@ -55,6 +55,7 @@ post '/callback' do
         # res.body['shop']がshopになる
         res = req.get(query)
         #puts res.body
+        puts res.body['results']['shop']
         puts res.body['shop']
         res.body["shop"].each_with_index do |shop, i|
           break if i == 3
