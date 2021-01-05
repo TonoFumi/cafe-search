5e89e9f39e2b624899b683a30ff57d34f0b23341
FumiakiTono <tonotorious@gmail.com>
2021-01-05 15:04:53 +0900

    修正

--- app.rb
+++ app.rb
@@ -41,7 +41,13 @@ post '/callback' do
         end
         master_query = URI.encode("/hotpepper/genre/v1/?key=#{ENV['HOTPEPPER_API_KEY']}&keyword=カフェ")
         res = req.get(master_query)
-        code = res['genre'][0]['code']
+        #code = res['genre'][0]['code']
+        code = res['genre']
+        message = {
+          type: 'text',
+          text: code
+        }
+        client.reply_message(event['replyToken'], message)
 
         # 緯度経度情報をホットペッパーAPIに投げ近くのカフェ情報をLINEクライアントに返す
         uri = "https://webservice.recruit.co.jp"
